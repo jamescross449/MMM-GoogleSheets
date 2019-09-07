623c4322fd52a6f6eb4bc2e4f213aa77bfa5b4a0
Jens den Braber <jens.denbraber@gmail.com>
2019-09-07 17:46:49 +0200

    Create MMM-GoogleSheets.js

---
+++ MMM-GoogleSheets.js
@@ -0,0 +1,12 @@
+/* Magic Mirror
+ * Module: MMM-GoogleSheets
+ *
+ * By ProgParty, https://github.com/Prog-Party/MMM-GoogleSheets
+ * MIT Licensed.
+ */
+
+Module.register('MMM-GoogleSheets', {
+	// Default configuration.
+	defaults: {
+  }
+}
